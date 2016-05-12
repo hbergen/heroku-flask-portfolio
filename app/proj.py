49096f9409e7b014838b416622df5604560cac7e
hbergen <heidi.a.whiting@gmail.com>
2016-05-11 18:44:07 -0700

    add port

--- app/proj.py
+++ app/proj.py
@@ -14,4 +14,5 @@ def home():
 	return render_template('home.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
